01ab0998c6f016f68d5afd1d1cb002a83cc0ece8
virtual <jeanine.mt@gmail.com>
2019-05-03 11:12:32 +0000

    change edit area demo

--- source/programs-courses.blade.php
+++ source/programs-courses.blade.php
@@ -21,6 +21,23 @@ Programs &amp; Courses
 @endsection
    
 @section('sidebar')
+
+<kbd class="ouc">Edit Sidebar</kbd>
+<p>Editable region; example: Alumni; does not allow edit of contact</p>
+
 @include('_partials/video')
-@include ('_partials/sidebar-contact')
+
+<!-- ! Driven by _sidebar.inc or similar -->
+<section class="modContactInfo">
+  <h2>Need Help?</h2> 
+  <h3>Admissions Center</h3><strong>Address</strong>SCC Central Campus, Student Services Building<br>Mail to: PO Box
+  4386, Spartanburg SC 29305<p><strong>Phone: </strong>(864) 592-4800</p>
+  <p><strong>Toll Free: </strong>(866) 591-3700</p>
+  <p><strong>Fax: </strong> (864) 592-4564</p>
+  <p><strong>E-mail:</strong><a href="mailto:admissions@sccsc.edu">admissions@sccsc.edu</a></p>
+  <p>Mon - Thu | 8:00am-6:00pm</p>
+  <p>Friday | 8:00am-5:00pm</p>
+  <p>&nbsp;</p>
+  <p><a href="/tours" title="Schedule a Tour of SCC">Schedule a Tour of SCC</a></p>
+</section>
 @endsection
