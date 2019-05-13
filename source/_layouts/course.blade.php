bfd4ca6562c619bb6723bbaa1d0e7642a955e20c
virtual <jeanine.mt@gmail.com>
2019-05-13 05:31:03 +0000

    update course for CE

--- source/_layouts/course.blade.php
+++ source/_layouts/course.blade.php
@@ -3,6 +3,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,6 +15,7 @@
   <meta property="og:title" content="@yield('pagetitle') : Spartanburg Community College" />
   @include('_partials/headcode')
 </head>
+
 <body>
   <!-- Google Tag Manager -->
   <!-- End Google Tag Manager -->
@@ -36,91 +38,104 @@
       </div>
       <div class="row-fluid">
 
-          <main class="span9 mainContent content-main">
-          <!-- ! Course - Detail Multiedit -->   
+        <main class="span9 mainContent content-main">
+          <!-- ! Course - Detail Multiedit -->
           <!-- https://www.sccsc.edu/client/coursedetail.aspx?courseID=534&programID=7828&majorID=7951 -->
-          <section class=" CourseDetail">
+          <section class="CourseDetail">
             <h2 class="sr-only">Course Information</h2>
-              <p class="coursedescription">This is a (college transfer) course in which the following topics are presented: a study of composition in conjunction with appropriate literary selections, with frequent theme assignments to reinforce effective writing. A review of standard usage and the basic techniques of research are also presented.</p>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Academic Level
-                    </div>
-                <div class=" span8">Undergraduate</div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Subject
-                    </div>
-                <div class=" span8">English</div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Department
-                    </div>
-                <div class=" span8">Humanities and Languages</div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Credits
-                    </div>
-                <div class=" span8">3</div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Prerequisites
-                    </div>
-                <div class=" span8">Take ENG-100 or ENG-165 and RDG-100 with a&nbsp;
-                          minimum grade C.&nbsp;
-                          </div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Corequisites
-                    </div>
-                <div class=" span8">
-                          None
-                        </div>
-              </div>
-              <div class="courseDetailData row-fluid">
-                <div class=" span4 courseDetailDataTitle">
-                      Course Syllabus
-                    </div>
-                <div class=" span8">
-                  <a href="https://lor1.sccsc.edu/syllabus/eng-101/" target="_blank">ENG 101</a>
-                </div>
-              </div>
-            </section>
+            <div class="coursedescription">
+              @yield('content')
+            </div>
 
 
-
-
-
-
-
-
-          </main>
-          <div class="span3 sideContent">	
-              @if (trim($__env->yieldContent('left-nav')))
-                <!-- Navigation - Inpage -->
-                @include ('_partials/left-nav')
-              @endif
-
-
-              <!-- Dynamically generated -->
-              <div class="programReturn"> 
-                <a href="/accounting/" class="button returnToProgram">Return to Accounting</a>
-              </div>
-
-              <!-- Asset: Apply Banner -->
-              <div>
-                <a href="https://sccsc.elluciancrmrecruit.com/Apply" target="_self" title="Apply now online" class="button apply">Apply Now!</a>
-              </div>
-              <!-- end Asset: Apply Banner -->
-
-              @yield('sidebar')
+            <div class="reginfo">
+              <p><strong>Classes Available</strong></p>
+              @yield('classinfo')
+              @yield('reginfo')
 
             </div>
+
+            <p class="alert">DO NOT SHOW THESE FIELDS IF CORRELATED INPUT IS EMPTY</p>
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Academic Level
+              </div>
+              <div class="span8">Undergraduate</div>
+            </div>
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Subject
+              </div>
+              <div class="span8">English</div>
+            </div> 
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Credits
+              </div>
+              <div class="span8">3</div>
+            </div>
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Prerequisites
+              </div>
+              <div class="span8">Take ENG-100 or ENG-165 and RDG-100 with a&nbsp;
+                minimum grade C.&nbsp;
+              </div>
+            </div>
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Corequisites
+              </div>
+              <div class="span8">
+                None
+              </div>
+            </div>
+            <div class="courseDetailData row-fluid">
+              <div class="span4 courseDetailDataTitle">
+                Cost
+              </div>
+              <div class="span8">
+                $380
+              </div>
+            </div>
+
+            <div class="paybutton text-center">
+              <p><a class="button apply" href="/client/TwoColumnCE.aspx?pageid=12884904761" target="_blank">Complete
+                  Policy Form then Pay Online</a></p>
+            </div>
+
+          </section>
+
+
+
+
+
+
+
+
+        </main>
+        <div class="span3 sideContent">
+          @if (trim($__env->yieldContent('left-nav')))
+          <!-- Navigation - Inpage -->
+          @include ('_partials/left-nav')
+          @endif
+
+
+          <!-- Dynamically generated -->
+          <div class="programReturn">
+            <a href="/accounting/" class="button returnToProgram">Return to Accounting</a>
+          </div>
+
+          <!-- Asset: Apply Banner -->
+          <div>
+            <a href="https://sccsc.elluciancrmrecruit.com/Apply" target="_self" title="Apply now online"
+              class="button apply">Apply Now!</a>
+          </div>
+          <!-- end Asset: Apply Banner -->
+
+          @yield('sidebar')
+
+        </div>
 
 
       </div><!-- / row-fluid -->
@@ -132,7 +147,8 @@
 
   <script src="{{ $page->baseUrl }}/assets/js/jquery.prettyPhoto.js"></script>
   <script src="{{ $page->baseUrl }}/assets/js/prettyPhoto-app.js"></script>
-  
+
   @yield('footcode')
 </body>
+
 </html>
--- source/course.blade.php
+++ source/course.blade.php
@@ -10,12 +10,23 @@ ENG-101 - English Composition I
  
 @section('content') 
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, explicabo expedita alias corrupti molestiae enim aspernatur consequatur illum est a beatae nemo eum voluptates vitae corporis ab ipsam incidunt dolorum.</p>
-<p><a href="https://www.goupstate.com/news/20181223/cambodian-immigrant-recounts-nursing-school-struggles" title="View article as it appears on GoUpstate.com">View article as it appears on GoUpstate.com</a></p>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, explicabo expedita alias corrupti molestiae enim aspernatur consequatur illum est a beatae nemo eum voluptates vitae corporis ab ipsam incidunt dolorum.</p>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, explicabo expedita alias corrupti molestiae enim aspernatur consequatur illum est a beatae nemo eum voluptates vitae corporis ab ipsam incidunt dolorum.</p>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, explicabo expedita alias corrupti molestiae enim aspernatur consequatur illum est a beatae nemo eum voluptates vitae corporis ab ipsam incidunt dolorum.</p>
 @endsection
-   
+
+@section('classinfo')
+<ul>
+  <li>Jul 29 - Aug 1 / Mon-Thur, 8:30am-5:30pm / Central Campus</li>
+</ul>
+@endsection
+
+@section('reginfo')
+<p>ALLOW FOR ASSET: Please
+  make a note of the class date you wish to register for. You will need 
+it to complete the Real Estate Policies &amp; Procedures form. After you
+  submit the form you will receive an email by the end of the next 
+business day with instructions regarding online registration and payment
+  for the class.
+</p>
+@endsection
 
 @section('sidebar')
   <kbd class="ouc">Edit Sidebar</kbd>
